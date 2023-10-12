940f9d051f1bc2726cf6ffb1798c4e2100829ea5
frostbite8283 <98437287+frostbite8283@users.noreply.github.com>
2023-10-11 23:57:17 -0400

    Added Menu

--- MI445-FA23-Proj2-Team3/Assets/UIManager.cs
+++ MI445-FA23-Proj2-Team3/Assets/UIManager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    List<GameObject> pages = new List<GameObject>();
+    int curPage;
+    private void Start()
     {
-        
+        curPage = 0;
+        SetPage(curPage);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPage(int page)
     {
-        
+        pages[curPage].SetActive(false);
+        pages[page].SetActive(true);
+        curPage = page;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 }
