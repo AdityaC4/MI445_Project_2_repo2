bcbcdc387a0e00859ac8ae4c51b5593a153734c8
Tybbi <60951119+Tybbi@users.noreply.github.com>
2023-10-17 01:59:52 -0400

    Logic for localized dialogue done
    - Set up the logic for localized dialogue in FMOD and the audio manager
    - Made banks for english and spanish localization
    - Edited the AudioManager in all scenes

--- MI445-FA23-Proj2-Team3/Assets/Scripts/Audio/AudioManager.cs
+++ MI445-FA23-Proj2-Team3/Assets/Scripts/Audio/AudioManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum eLanguage { ENG, SPN };
+
+[RequireComponent(typeof(FMODDialogueCallback))]
 public class AudioManager : MonoBehaviour
 {
     // Singleton //
-    public AudioManager S;
+    static public AudioManager S;
 
     // VCAs //
     FMOD.Studio.VCA master;
@@ -13,27 +16,48 @@ public class AudioManager : MonoBehaviour
     FMOD.Studio.VCA dia;
 
     // Snapshots // 
-    static FMOD.Studio.EventInstance insideLockerSnapshot;
+    private FMOD.Studio.EventInstance insideLockerSnapshot;
+
+    // Other //
+    FMODDialogueCallback dialogueCallback;
+
+    [Header("Audio Options [ do NOT edit plz >:( ]")]
+    [SerializeField]
+    private eLanguage currentLanguage = eLanguage.ENG;
+
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.LogWarning("Already have an audiomanager. Deleting this boi");
+            Destroy(this);
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
-        if (S != null)
-        {
-            S = this;
-        }
-
-        // Get snapshot
+        // snapshots
         insideLockerSnapshot = FMODUnity.RuntimeManager.CreateInstance("snapshot:/Inside Locker");
         insideLockerSnapshot.setParameterByName("Intensity", 0);
-        float ahh;
-        insideLockerSnapshot.getParameterByName("Intensity", out ahh);
-        Debug.Log(ahh);
         insideLockerSnapshot.start();
 
-        // MASTER BANK IS LOADED ON GAME INIT. ALL OTHERS SHOULD BE DONE MANUALLY
+        // MASTER BANK IS LOADED ON GAME INIT. ALL OTHERS SHOULD BE DONE MANUALLY //
+        // Look for player's selected language & load that bank todo comment back in line below
+        //currentLanguage = (eLanguage) PlayerPrefs.GetInt("Language", (int)eLanguage.ENG);
+        LoadLanguageBank(currentLanguage);
+
+        // Create dialogue callback boi
+        dialogueCallback = GetComponent<FMODDialogueCallback>();
+        dialogueCallback.PlayDialogue("Test");
     }
 
-    static public void toggleInsideLockerSnapshot(bool on)
+    public void toggleInsideLockerSnapshot(bool on)
     {
         if (on)
         {
@@ -44,5 +68,24 @@ public class AudioManager : MonoBehaviour
         {
             insideLockerSnapshot.setParameterByName("Intensity", 0);
         }
+    }
+
+    public void LoadLanguageBank(eLanguage language)
+    {
+        // Unload current bank
+        FMODUnity.RuntimeManager.UnloadBank("LocalizedDIA_" + currentLanguage.ToString());
+
+        // Load new one
+        FMODUnity.RuntimeManager.LoadBank("LocalizedDIA_" + language.ToString());
+        PlayerPrefs.SetInt("Language", (int) currentLanguage);
+    }
+
+    /// <summary>
+    /// Pretty much just a passthrough for the dialogue callback
+    /// </summary>
+    /// <param name="key">The audio file to play</param>
+    public void PlayDialogue(string key)
+    {
+        dialogueCallback.PlayDialogue(key);
     }
 }
--- MI445-FA23-Proj2-Team3/Assets/Scripts/Controller/PlayerController.cs
+++ MI445-FA23-Proj2-Team3/Assets/Scripts/Controller/PlayerController.cs
@@ -111,7 +111,7 @@ public class PlayerController : MonoBehaviour
         //transform.rotation = rotation;
         GetComponent<Rigidbody>().detectCollisions = false;
         FMODUnity.RuntimeManager.PlayOneShot("event:/Locker Enter + Exit");
-        AudioManager.toggleInsideLockerSnapshot(true);
+        AudioManager.S.toggleInsideLockerSnapshot(true);
     }
 
     public void ExitLocker(Vector3 endPos)
@@ -127,7 +127,7 @@ public class PlayerController : MonoBehaviour
         GetComponent<Rigidbody>().detectCollisions = true;
         canMove = true;
         FMODUnity.RuntimeManager.PlayOneShot("event:/Locker Enter + Exit");
-        AudioManager.toggleInsideLockerSnapshot(false);
+        AudioManager.S.toggleInsideLockerSnapshot(false);
     }
 
     void FlashSwitch()
